7e97be43699af144bfdde0bb8a8fe52554b1380f
AmelyanenkaViktoryia <vedrussa1@yandex.ru>
2019-02-26 21:59:58 +0300

    unit tests

--- frontend/frontcamp/src/app/full-article/full-article.component.spec.ts
+++ frontend/frontcamp/src/app/full-article/full-article.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Article } from '../article.interface';
 import { FullArticleComponent } from './full-article.component';
 
 describe('FullArticleComponent', () => {
@@ -22,4 +22,24 @@ describe('FullArticleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(FullArticleComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`Should have article property typed Article`, () => {
+    const fixture = TestBed.createComponent(FullArticleComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(typeof(app.article)).toEqual('Article');
+  });
+
+  it('Should display No Data', () => {
+    const fixture = TestBed.createComponent(FullArticleComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.no-data').textContent).toContain('No data. Select Source');
+  });
+
 });
